63c4bd13f124d86ed533491c70f793ffbd97d6c9
Odiljonov <Odiljonov@gmail.com>
2026-07-01 22:28:33 +0500

    2-qadam: Arifmetik funksiyalar qo'shildi

--- functions.py
+++ functions.py
@@ -8,3 +8,30 @@ def kvadrat(a):
 
 def kub(a):
     return a * a * a
+
+def hello():
+    print("Salom!")
+
+
+def kvadrat(a):
+    return a * a
+
+
+def kub(a):
+    return a * a * a
+
+
+def yigindi(a, b):
+    return a + b
+
+
+def ayirma(a, b):
+    return a - b
+
+
+def kopaytma(a, b):
+    return a * b
+
+
+def bolish(a, b):
+    return a / b
