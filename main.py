add71e8d85a97bbead5cc4adabeca1960597b01e
Odiljonov <Odiljonov@gmail.com>
2026-07-01 22:16:45 +0500

    Katta sonni topish funksiyasi qo'shildi

--- main.py
+++ main.py
@@ -13,3 +13,15 @@ def kopaytirish(a, b):
 salom("Abbos")
 print("Yig'indi:", qoshish(10, 5))
 print("Ko'paytma:", kopaytirish(4, 6))
+
+def kvadrat(x):
+    return x * x
+
+print(kvadrat(9))
+
+def katta_son(a, b):
+    if a > b:
+        return a
+    return b
+
+print(katta_son(25, 18))
