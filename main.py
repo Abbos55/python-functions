9aef72b6624ffff6e29e7a571712c698528385b0
Odiljonov <Odiljonov@gmail.com>
2026-07-01 22:07:13 +0500

    Birinchi funksiya qo'shildi

---
+++ main.py
@@ -0,0 +1,15 @@
+def salom(ism):
+    print(f"Salom, {ism}!")
+
+
+def qoshish(a, b):
+    return a + b
+
+
+def kopaytirish(a, b):
+    return a * b
+
+
+salom("Abbos")
+print("Yig'indi:", qoshish(10, 5))
+print("Ko'paytma:", kopaytirish(4, 6))
